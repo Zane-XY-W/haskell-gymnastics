34e780926040beaf87768474a95e423ae2894d66
Zane.XY <zane.xy@live.com>
2014-02-08 21:36:08 +0800

    update recursion

--- test/RecursionSpec.hs
+++ test/RecursionSpec.hs
@@ -4,14 +4,15 @@ module RecursionSpec where
 import           Recursion
 import           Test.Hspec
 import           Test.QuickCheck
-import           Test.QuickCheck.Property
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec = describe "sumRange" $
-    it "should be the same as sum list" $
-      sumRange 1 7 2 `shouldBe` sum [1,3..7]
-    -- it "should b the same as sum list function" $
-    --   property $ \x y -> sumRange x y 1 == sum [x..y]
+spec = describe "sumRange" $ do
+  describe "sumRange1" $ do
+    it "should be the same as sum list" $ property $ do
+     sumRange 1 7 2 == sum [1,3..7]
+  describe "sumRange2" $ do
+    it "should be the same as sum list function" $ property $
+      \(x::Int) (y::Int) -> (x < y) ==> sumRange x y 1 == sum [x..y]
