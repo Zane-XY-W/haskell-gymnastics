c406b1ccd3029118e0633d08cfeaf8e14d7f83a7
Zane.XY <zane.xy@live.com>
2014-03-18 23:23:32 +0800

    add zipper

---
+++ src/Zipper/TreeZipper.hs
@@ -0,0 +1,33 @@
+module TreeZipper where
+
+-- | a tree is either a fork or a leaf
+data Tree a = Fork (Tree a) (Tree a) | Leaf a
+
+-- | a context has a hole at either left or right, and a context or a path
+-- to root
+data Cxt a = Top | L (Cxt a) (Tree a) | R (Tree a) (Cxt a)
+
+-- | a Loc forms a whole tree, subtree + context
+type Loc a = (Tree a, Cxt a)
+
+-- | go to left of a fork, Fork also represents the focus
+-- go left result in a hole in the left, a context has a hole in the left
+-- is L c r
+left :: Loc a -> Loc a
+left (Fork l r, c) = (l, L c r)
+
+-- | go to right of a fork
+right :: Loc a -> Loc a
+right (Fork l r, c) = (r, R l c)
+
+-- | takes a tree, and returns a Loc with focus at the top
+top :: Tree a -> Loc a
+top t = (t, Top)
+
+-- | up means moves hole to a upper level,
+-- if the hole has a right sibling r, then after moveing the hole up,
+-- the subtree attached to the hole becomes the siblings of r,
+-- that's why Fork t r make sense here. but I think the c should also change?
+up :: Loc a -> Loc a
+up (t, L c r) = (Fork t r, c)
+up (t, R l c) = (Fork l t, c)
