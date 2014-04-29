9144a7333fa177b2f97af69984b1dc0e38bbf3bf
Zane.XY <zane.xy@live.com>
2014-04-29 18:09:20 +0800

    reorganize Reader Monad example

--- src/Reader/ReaderPrac.hs
+++ src/Reader/ReaderPrac.hs
@@ -14,43 +14,42 @@ configs = Map.fromList [("count",3), ("1",1), ("b",2)]
 
 -- Returns True if the "count" variable contains correct bindings size.
 isCountCorrect :: Config -> Bool
-isCountCorrect bindings = runReader checkCount bindings
+isCountCorrect = runReader checkCount
 
 -- The Reader monad, which implements this complicated check.
 checkCount :: Reader Config Bool
-checkCount = do
-    count <- asks (lookupVar "count")
-    bindings <- ask
-    return (count == (Map.size bindings))
+checkCount
+  = do count <- asks (lookupVar "count")
+       confMap <- ask
+       return (count == (Map.size confMap))
 
 -- The selector function to  use with 'asks'.
 -- Returns value of the variable with specified name.
 lookupVar :: String -> Config -> Int
 lookupVar name bindings = fromJust (Map.lookup name bindings)
 
-
 getConfigNums :: Reader String Int
-getConfigNums = do
-    content <- ask
-    return (length content);
+getConfigNums
+  = do content <- ask
+       return (length content)
 
--- Calls calculateContentLen after adding a prefix to the Reader content.
+--|
+-- local: Executes a computation in a modified environment
 calculateModifiedContentLen :: Reader String Int
 calculateModifiedContentLen = local ("Prefix " ++) getConfigNums
 
 -- The Reader/IO combined monad, where Reader stores a string.
 printReaderContent :: ReaderT String IO ()
-printReaderContent = do
-    content <- ask
-    liftIO $ putStrLn ("The Reader Content: " ++ content)
+printReaderContent
+  = do content <- ask
+       liftIO $ putStrLn ("The Reader Content: " ++ content)
 
 prop_isCountCorrect = isCountCorrect configs == True
 
-prop_local = do
-    let s = "12345";
-    let modifiedLen = runReader calculateModifiedContentLen s
-    let len = runReader getConfigNums s
-    modifiedLen == len 
-
+prop_local
+  = do let s = "12345"
+       let modifiedLen = runReader calculateModifiedContentLen s
+       let len = runReader getConfigNums s
+       modifiedLen == len
 
 main = $quickCheckAll
